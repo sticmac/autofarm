c6fb48a2c31ec5360796043654209cfc2bc95f1f
TsukiShinen <49118528+TsukiShinen@users.noreply.github.com>
2022-03-19 23:23:38 +0100

    Update Actuator.cs

--- Assets/Scripts/Actuator.cs
+++ Assets/Scripts/Actuator.cs
@@ -5,19 +5,41 @@ using UnityEngine;
 public abstract class Actuator
 {
     public string Name;
+    public List<Rule> Rules;
+    public bool _activated;
 
     public Actuator(string name)
     {
         Name = name;
+        _activated = false;
+    }
+
+    public void SearchAction()
+    {
+        if (_activated) { return; }
+        foreach (var rule in Rules)
+        {
+            if (rule.verifyRule())
+            {
+                if (rule.action == Rule.Actions.Activate)
+                {
+                    Activate();
+                } else
+                {
+                    Desactivate();
+                }
+                return;
+            }
+        }
     }
 
     public virtual void Activate()
     {
-
+        _activated = true;
     }
 
     public virtual void Desactivate()
     {
-
+        _activated = false;
     }
 }
